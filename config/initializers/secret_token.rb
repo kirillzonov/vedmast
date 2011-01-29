0da7d1aa4157120e17d9518aa0d0a011d05dbdb8
Kirill Zonov <graffzon@gmail.com>
2011-01-29 21:32:59 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obereg::Application.config.secret_token = 'df67c2fac6577ee41de0175b882505ac14a244d1e703ae3c853d5e39b0215ad2d48a92589cacdb1305b1e6c0556805a8bd9ff5d38f052ce002c8e98e3c05463e'
