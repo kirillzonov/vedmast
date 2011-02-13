8f7427f022e86714dfd54abe29a41b546be6cc68
Kirill Zonov <graffzon@gmail.com>
2011-02-13 23:18:04 +0300

    little fix in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @news = News.all
     @main = RedCloth.new(Main.last.main).to_html
     respond_to do |format|
       format.html # index.html.erb
