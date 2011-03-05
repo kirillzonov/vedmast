a5252cd09c9e155f7471cb2aaa3d4a9aa0fb2eed
Kirill Zonov <graffzon@gmail.com>
2011-03-05 20:32:38 +0300

    fixed add category

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,8 @@ class PagesController < ApplicationController
   def index
     @index_html = RedCloth.new(Page.find_by_name('index').content).to_html 
     @index = Page.find_by_name('index')
-    @delivery_html = RedCloth.new(Page.find_by_name('delivery').content).to_html 
-    @delivery = Page.find_by_name('delivery')
+#    @delivery_html = RedCloth.new(Page.find_by_name('delivery').content).to_html 
+#    @delivery = Page.find_by_name('delivery')
 #    @admin_index = RedCloth.new(Page.find_by_name('admin_index').content).to_html 
   end
 
