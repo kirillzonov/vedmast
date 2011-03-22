a439f3f27b36e09fcd655a6a79dbf59d7dcc0f64
Kirill Zonov <graffzon@gmail.com>
2011-03-22 11:00:16 +0300

    центрирование новинок, разные фоны для разных страниц

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def index_or_not(ind = false)
+    if ind
+      render :inline => '<div class= "wrap for-content home">'
+    else
+      render :inline => '<div class= "wrap for-content static about">'
+    end
+  end
 end
