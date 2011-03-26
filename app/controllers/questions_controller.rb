f995d33b67dc509944074d940ba512bcf4557915
Kirill Zonov <graffzon@gmail.com>
2011-03-26 23:45:30 +0300

    mailers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,4 +11,32 @@ class QuestionsController < ApplicationController
       format.xml  { render :xml => @questions }
     end
   end
+ def create
+    @question = Question.new(params[:question])
+
+    respond_to do |format|
+      if @question.save
+        QuestionMailer.new_question(@question).deliver
+        format.html { redirect_to(questions_path, :notice => 'Question was successfully created.') }
+        format.xml  { render :xml => @question, :status => :created, :location => @question }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+   def update
+    @question = Question.find(params[:id])
+
+    respond_to do |format|
+      if @question.update_attributes(params[:question])
+        QuestionMailer.new_comment(@question).deliver
+        format.html { redirect_to(@question, :notice => 'Question was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
