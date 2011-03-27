e8d4a5aad957795eef13d367bb1b27589942ec28
Kirill Zonov <graffzon@gmail.com>
2011-03-27 15:05:43 +0400

    paginate information

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -3,7 +3,7 @@ class InformationController < ApplicationController
   # GET /information
   # GET /information.xml
   def index
-    @information = Information.all.paginate(:page => params[:page], :per_page => 3)
+    @information = Information.all.page(params[:page]),per(3)
     @index_html = RedCloth.new(Page.find_by_name('index').content).to_html 
 
     respond_to do |format|
