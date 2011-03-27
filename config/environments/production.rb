8dd38fcd81836e72a22c1a4d6d32a91970e9f9b1
Kirill Zonov <graffzon@gmail.com>
2011-03-27 20:48:56 +0400

    config action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Obereg::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address => "smtp.gmail.com" ,
+  :port => 587,
+#  :domain => " domain.of.sender.net " ,
+  :authentication => "plain" ,
+  :user_name => "drevnmast@gmail.com" ,
+  :password => "vfcrvfcr" ,
+  :enable_starttls_auto => true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
