a4dc3e6aba5abaf514027a4d9e34cb7cf6097bbe
Kirill Zonov <kirillzonov@flatsoft.com>
2011-04-21 23:10:30 +0400

    image uploader for cat and info

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :articles
   validates_presence_of :name
+  mount_before_save_uploader :name_pic, ImageUploader
 end
--- app/models/information.rb
+++ app/models/information.rb
@@ -1,3 +1,4 @@
 class Information < ActiveRecord::Base
   validates_presence_of :name, :content
+  mount_before_save_uploader :name_pic, ImageUploader
 end
